d8c46437caae69328f8feb7de78689e08cf25113
Kimcheeee <102534231+Kimcheeee@users.noreply.github.com>
2022-07-07 14:23:16 +0100

    Added first item and creative tab (No textures yet)

--- src/main/java/com/kimcheee/mercuria/Mercuria.java
+++ src/main/java/com/kimcheee/mercuria/Mercuria.java
@@ -1,12 +1,37 @@
 package com.kimcheee.mercuria;
 
+import com.kimcheee.mercuria.init.ItemInit;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("mercuria")
 public class Mercuria {
 	
+	public static final String MOD_ID = "mercuria";
+	
+	// Adds a creative tab
+	public static final CreativeModeTab MERCURIA_TAB = new CreativeModeTab(MOD_ID) {
+		
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public ItemStack makeIcon() {
+			// TODO Auto-generated method stub
+			return new ItemStack(ItemInit.EXAMPLE_ITEM.get());
+		}
+	};
+	
 	public Mercuria() {
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		
+		ItemInit.ITEMS.register(bus);
+		
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 }
